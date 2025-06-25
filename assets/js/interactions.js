45541026bf7270eabde559cb1c5cef51b5fcb7d6
AnuTech001 <ruiniming10@gmail.com>
2025-06-25 19:11:28 -0300

    Agumas melhorias no mobile

---
+++ assets/js/interactions.js
@@ -0,0 +1,8 @@
+/* function toggleOpecoes() {
+    const bloco = document.getElementById("bloco-opecoes");
+    const botao = event.target;
+
+    const mostrando = bloco.style.display === "block";
+    bloco.style.display = mostrando ? "none" : "block";
+    botao.textContent = mostrando ? "Mais informações" : "Menos informações";
+} */
