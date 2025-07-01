60fef23f917b93c70651cc3bfb12c7aeb8e64b54
AnuTech001 <ruiniming10@gmail.com>
2025-06-30 23:47:43 -0300

    Subindo melhorias e um novo recurso de acessibilidade da versão 1.4.3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const loadHTML = (id, file) => {
+    console.log(`Carregando: ${file}`); // <- move pra cá
     fetch(file)
       .then((response) => response.text())
       .then((data) => (document.getElementById(id).innerHTML = data))
@@ -9,4 +10,5 @@ document.addEventListener("DOMContentLoaded", function () {
   loadHTML("header", "components/header.html");
   loadHTML("content", "components/content.html");
   loadHTML("footer", "components/footer.html");
+  loadHTML("head_links", "components/includes/head_links.html");
 });
