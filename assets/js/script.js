98794206f35ada29829fd65867fd8f888c8e060f
AnuTech001 <ruiniming10@gmail.com>
2025-10-13 21:32:17 -0300

    Subindo versão 2.0.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,21 @@
-const loadHTML = (id, file) => {
-  console.log(`Carregando: ${file}`);
-  return fetch(file)
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById(id).innerHTML = data;
-    });
-};
+function atualizarDimensoes() {
+  const elemento = document.getElementById("dimensoes-tela");
+  elemento.textContent = `Largura: ${window.innerWidth}px × Altura: ${window.innerHeight}px`;
+}
 
-// Aguarda o carregamento de todos
-document.addEventListener("DOMContentLoaded", function () {
-  Promise.all([
-    /* loadHTML("span", "components/span.html"), */
-    loadHTML("header", "components/header.html"),
-    loadHTML("anuzita", "components/anuzita.html"),
-    loadHTML("content", "components/content.html"),
-    loadHTML("navegacao_rapida", "components/navegacao_rapida.html"),
-    loadHTML("head_links", "components/includes/head_links.html"),
-    loadHTML("sites", "components/sites.html"),
-    loadHTML("projetos", "components/projetos.html"),
-    loadHTML("computacao_quantica", "components/computacao_quantica.html"),
-    loadHTML("area-dev", "components/area-dev.html"),
-    loadHTML("manifesto", "components/manifesto.html"),
-    loadHTML("rodape", "components/rodape.html"),
-  ])
-    .then(() => {
-      console.log("Todos os componentes carregados!");
-      mostrarCaixaTamanho();
-      window.addEventListener("resize", mostrarCaixaTamanho);
-    })
-    .catch((error) => console.error("Erro ao carregar componentes:", error));
+window.addEventListener("resize", atualizarDimensoes);
+
+atualizarDimensoes();
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("assets/py/assets/data/linhas.json")
+    .then((res) => res.json())
+    .then((data) => {
+      const linhas = document.getElementById("linhas");
+      if (linhas) {
+        linhas.innerText = `${data.total} linhas de código`;
+      } else {
+        console.warn("Elemento com id 'linhas' não encontrado.");
+      }
+    });
 });
